f0fb2fa9793008d48f53d800cb0c25fb8b9224ed
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2022-11-14 14:49:41 +0530

    fixed: protected route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CommonBar from "./components/CommonBar/CommonBar.jsx"
+import CommonBar from "./components/CommonBar/CommonBar.jsx";
+import ProtectedRoute, {
+  NormalUserProtectedRoute,
+} from "./components/ProtectedRoute/ProtectedRoute.jsx";
 import SnackBar from "./components/SnackBar/SnackBar.jsx";
 import ClientView from "./pages/ClientView/ClientView.jsx";
 import Feedback from "./pages/Feedback/Feedback.jsx";
@@ -14,15 +17,14 @@ import Register from "./pages/Register/Register.jsx";
 import Settings from "./pages/SettingsView/Settings.jsx";
 
 function App() {
-
   const [userInfo, setUserInfo] = useState({
     fname: null,
     lname: null,
     email: null,
-    profileImgUrl:null,
+    profileImgUrl: null,
     userType: null,
     state: null,
-  })
+  });
 
   // console.log("User first name from app js firl is " + userInfo.fname);
 
@@ -30,21 +32,56 @@ function App() {
 
   return (
     <div>
-      <SnackBar/>
+      <SnackBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login setUserInfo={setUserInfo} />} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/home" element={<CommonBar title={"Home"} component={<Home/>} />} />
-          <Route path="/client-view" element={<CommonBar title={"Client View"} component={<ClientView/>} />} />
-          <Route path="/promoter-view" element={<CommonBar title={"Promoter View"} component={<PromoterView/>} />} />
-          <Route path="/payments" element={<CommonBar title={"Payments"} component={<Payments/>} />} />
-          <Route path="/settings" element={<CommonBar title={"Settings"} component={<Settings/>} />} />
-          <Route path="/help" element={<CommonBar title={"Help"} component={<Help/>} />} />
-          <Route path="/feedback" element={<CommonBar title={"Feedback"} component={<Feedback userInfo={userInfo}/>} />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <NormalUserProtectedRoute>
+                  <CommonBar title={"Home"} component={<Home />} />
+                </NormalUserProtectedRoute>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/client-view"
+            element={
+              <CommonBar title={"Client View"} component={<ClientView />} />
+            }
+          />
+          <Route
+            path="/promoter-view"
+            element={
+              <CommonBar title={"Promoter View"} component={<PromoterView />} />
+            }
+          />
+          <Route
+            path="/payments"
+            element={<CommonBar title={"Payments"} component={<Payments />} />}
+          />
+          <Route
+            path="/settings"
+            element={<CommonBar title={"Settings"} component={<Settings />} />}
+          />
+          <Route
+            path="/help"
+            element={<CommonBar title={"Help"} component={<Help />} />}
+          />
+          <Route
+            path="/feedback"
+            element={
+              <CommonBar
+                title={"Feedback"}
+                component={<Feedback userInfo={userInfo} />}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
-    
     </div>
   );
 }
---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { showAlert } from "../../store/reducers/alert.slice";
+import { store } from "../../store/store";
+
+const NORMAL_USER = "NORMAL_USER";
+const ADMIN_USER = "ADMIN_USER";
+
+const ERROR_MESSAGE = "You are not authorized to that page";
+
+// check whether the user is logged into the system
+const ProtectedRoute = ({children}) => {
+    const userInfo = useSelector((state) => state.login);
+    const [shouldReturn, setShouldReturn] = useState(false);
+
+    const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+
+    useEffect(() => {
+        if (!isLoggedIn){
+            store.dispatch(
+                showAlert({
+                    message: "Please login to the system",
+                    isVisible: true,
+                    severity: "warning",
+                })
+            );
+            setShouldReturn(true);
+        }
+    }, [userInfo, isLoggedIn]);
+    if(shouldReturn){
+        return <Navigate to="/" replace />
+    }
+    return children;
+};
+
+export const NormalUserProtectedRoute = ({children}) => {
+    const userType = useSelector((state) => state.login.userType);
+    if (userType !== NORMAL_USER){
+        store.dispatch(
+            showAlert({
+                message: ERROR_MESSAGE,
+                isVisible: true,
+                severity: "warning",
+            })
+        );
+        return <Navigate to="/" replace />
+    }
+    return children;
+};
+
+export default ProtectedRoute;
